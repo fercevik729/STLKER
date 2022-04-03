d398eae43dbef7760981e2513adc85557e1774c9
fercevik729 <ercevik.furkan@gmail.com>
2022-04-03 16:04:08 -0700

    Updated envoy configs and created an index.html file to test proxy

--- watcher-api/main.go
+++ watcher-api/main.go
@@ -3,13 +3,11 @@ package main
 import (
 	"log"
 	"net"
-	"net/http"
 	"os"
 
 	"github.com/fercevik729/STLKER/watcher-api/data"
 	server "github.com/fercevik729/STLKER/watcher-api/go-server"
 	pb "github.com/fercevik729/STLKER/watcher-api/protos"
-	"github.com/improbable-eng/grpc-web/go/grpcweb"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
@@ -31,21 +29,7 @@ func main() {
 	}
 	l.Println("[DEBUG] gRPC server listening at localhost:9092")
 
-	go func() {
-		// Listen on grpc server
-		log.Fatal("failed to serve:", gs.Serve(lis))
-	}()
+	// Listen on grpc server
+	log.Fatal("failed to serve:", gs.Serve(lis))
 
-	// Create grpc web server
-	grpcWebServer := grpcweb.WrapServer(
-		gs, grpcweb.WithOriginFunc(func(origin string) bool { return true }),
-	)
-
-	// Listen on web server
-	srv := &http.Server{
-		Handler: grpcWebServer,
-		Addr:    ":9093",
-	}
-	l.Println("[DEBUG] http server listening at localhost:9093")
-	l.Fatalln("failed to serve:", srv.ListenAndServe())
 }
