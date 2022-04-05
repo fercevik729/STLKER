006c7334a17c2010c268a497937b34cfd3707f36
fercevik729 <ercevik.furkan@gmail.com>
2022-04-05 12:57:08 -0700

    Tried creating a react app to communicate with grpc api but unresponsive

--- watcher-api/main.go
+++ watcher-api/main.go
@@ -14,6 +14,23 @@ import (
 	"google.golang.org/grpc/reflection"
 )
 
+type grpcMux struct {
+	*grpcweb.WrappedGrpcServer
+}
+
+// Handler routes requests to grpc or the regular http server
+func (m *grpcMux) Handler(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if m.IsGrpcWebRequest(r) {
+			m.ServeHTTP(w, r)
+			return
+		} else if m.IsAcceptableGrpcCorsRequest(r) {
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+		}
+		//next.ServeHTTP(w, r)
+	})
+}
 func main() {
 	l := log.New(os.Stdout, "watcher-api", log.LstdFlags)
 
@@ -37,14 +54,27 @@ func main() {
 		log.Fatalf("failed to serve: %v\n", gs.Serve(lis))
 	}()
 
-	// create gRPC web server with CORS enabled
-	grpcWebServer := grpcweb.WrapServer(gs, grpcweb.WithOriginFunc(func(origin string) bool { return false }))
+	// Create gRPC web server with CORS enabled
+	grpcWebServer := grpcweb.WrapServer(gs,
+		grpcweb.WithAllowedRequestHeaders([]string{"*"}),
+		grpcweb.WithCorsForRegisteredEndpointsOnly(false),
+		grpcweb.WithOriginFunc(func(origin string) bool {
+			return origin == "http://localhost:8080" || origin == "localhost:9090" || origin == "localhost:3000"
+		}))
+	mux := grpcMux{grpcWebServer}
 
-	l.Println("[DEBUG] http server listening at 0.0.0.0:9091")
+	// Create http router and fileserver
+	r := http.NewServeMux()
+	webapp := http.FileServer(http.Dir("ui/stlkerapp/build"))
 
+	// Register handlers
+	r.Handle("/", mux.Handler(webapp))
+
+	// Start up http server
+	l.Println("[DEBUG] http server listening at 0.0.0.0:8080")
 	srv := &http.Server{
 		Handler: grpcWebServer,
-		Addr:    "0.0.0.0:9091",
+		Addr:    "0.0.0.0:8080",
 	}
 	// Listen on grpc server
 	log.Fatal("failed to serve:", srv.ListenAndServe())
